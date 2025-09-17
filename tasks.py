986946e37cfbfa3f22b69d98f1b667dd06c33404
Bondarev Maxim <bonmax46@mail.ru>
2025-09-17 22:01:00 +0300

    add add

--- tasks.py
+++ tasks.py
@@ -7,7 +7,7 @@ celery_app = Celery(broker='redis://127.0.0.1:6379/1', backend='redis://127.0.0.
 
 @celery_app.task
 def cpu_bound_function(a, b):
-    a + b
+    s = a + b
     time.sleep(2)
 
-    return 'Hello World'
+    return s
